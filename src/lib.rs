596371506de8393946523b16d730a5db106a9a28
Orson Peters <orsonpeters@gmail.com>
2024-03-10 14:22:25 +0100

    Polish lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,6 @@
 use core::cmp::{self, Ordering};
 use core::intrinsics;
 use core::mem::{self, MaybeUninit, SizedTypeProperties};
-use core::ptr;
-use core::slice;
 
 mod drift;
 mod merge;
@@ -59,7 +57,7 @@ fn driftsort<T, F: FnMut(&T, &T) -> bool, BufT: BufGuard<T>>(v: &mut [T], is_les
     // binary size of insertion sort is more important. The instruction cache in
     // modern processors is very valuable, and for a single sort call in general
     // purpose code any gains from an advanced method are cancelled by icache
-    // misses during the sort, and thrashing the icache for surrounding code.
+    // misses during the sort, and thrashing the i-cache for surrounding code.
     const MAX_LEN_ALWAYS_INSERTION_SORT: usize = 20;
     if intrinsics::likely(len <= MAX_LEN_ALWAYS_INSERTION_SORT) {
         smallsort::insertion_sort_shift_left(v, 1, is_less);
@@ -69,74 +67,55 @@ fn driftsort<T, F: FnMut(&T, &T) -> bool, BufT: BufGuard<T>>(v: &mut [T], is_les
     driftsort_main::<T, F, BufT>(v, is_less);
 }
 
-// Deliberately don't inline the core logic to ensure the inlined insertion sort i-cache footprint
-// is minimal.
+// Deliberately don't inline the main sorting routine entrypoint to ensure the
+// inlined insertion sort i-cache footprint remains minimal.
 #[inline(never)]
 fn driftsort_main<T, F: FnMut(&T, &T) -> bool, BufT: BufGuard<T>>(v: &mut [T], is_less: &mut F) {
     use crate::smallsort::SmallSortTypeImpl;
 
-    // Allocating len instead of len / 2 allows the quicksort to work on the full size, which can
-    // give speedups especially for low cardinality inputs where common values are filtered out only
-    // once, instead of twice. And it allows bi-directional merging the full input. However to
-    // reduce peak memory usage for large inputs, fall back to allocating len / 2 if a certain
-    // threshold is passed.
+    // By allocating n elements of memory we can ensure the entire input can
+    // be sorted using stable quicksort, which allows better performance on
+    // random and low-cardinality distributions. However, we still want to
+    // reduce our memory usage to n / 2 for large inputs. We do this by scaling
+    // our allocation as max(n / 2, min(n, 8MB)), ensuring we scale like n for
+    // small inputs and n / 2 for large inputs, without a sudden dropoff. We
+    // also need to ensure our alloc >= MIN_SMALL_SORT_SCRATCH_LEN, as the
+    // small-sort always needs this much memory.
     const MAX_FULL_ALLOC_BYTES: usize = 8_000_000; // 8MB
-
-    // Pick whichever is greater:
-    //
-    //  - alloc n up to MAX_FULL_ALLOC_BYTES
-    //  - alloc n / 2
-    //
-    // This serves to make the impact and performance cliff when going above the threshold less
-    // severe than immediately switching to len / 2.
+    let max_full_alloc = MAX_FULL_ALLOC_BYTES / mem::size_of::<T>();
     let len = v.len();
-    let full_alloc_len = cmp::min(len, MAX_FULL_ALLOC_BYTES / mem::size_of::<T>());
-    let alloc_len = cmp::max(
-        cmp::max(len / 2, full_alloc_len),
-        crate::smallsort::MIN_SMALL_SORT_SCRATCH_LEN,
-    );
+    let alloc_len = cmp::max(len / 2, cmp::min(len, max_full_alloc));
+    let alloc_len = cmp::max(alloc_len, crate::smallsort::MIN_SMALL_SORT_SCRATCH_LEN);
 
-    // Tiny inputs N <= MAX_LEN_ALWAYS_INSERTION_SORT will be sorted by insertion sort without
-    // allocating. Which is followed by a transition to the core hybrid merge- quicksort algorithm.
-    // The combined impact of large amounts of new code that needs to enter the i-cache, dozens of
-    // branches for control logic and a call to the allocator with a min of len + 16 for small
-    // inputs, yields a sharp regression in throughput on many machines. Especially in cold
-    // benchmarks. Using stack allocation avoids substantial overhead and potential contention on
-    // the global allocator.
-    let mut stack_array = AlignedStorage::<T, 4096>::new();
-    let stack_array_scratch = stack_array.as_mut();
-    let mut buf;
-
-    let scratch = if stack_array_scratch.len() >= alloc_len {
-        stack_array_scratch
+    // For small inputs 4KiB of stack storage suffices, which allows us to avoid
+    // calling the (de-)allocator. Benchmarks showed this was quite beneficial.
+    let mut stack_buf = AlignedStorage::<T, 4096>::new();
+    let stack_scratch = stack_buf.as_mut_uninit_slice();
+    let mut heap_buf;
+    let scratch = if stack_scratch.len() >= alloc_len {
+        stack_scratch
     } else {
-        buf = BufT::with_capacity(alloc_len);
-        // SAFETY: We rely on the `BufT` implementation to return valid memory of `alloc_len`.
-        unsafe { slice::from_raw_parts_mut(buf.mut_ptr() as *mut MaybeUninit<T>, buf.capacity()) }
+        heap_buf = BufT::with_capacity(alloc_len);
+        heap_buf.as_mut_uninit_slice()
     };
 
-    // Using the hybrid quick + merge-sort has performance issues with the transition from insertion
-    // sort to main loop. A more gradual and smoother transition can be had by using an always eager
-    // merge approach as long as it can be served by a single merge.
+    // For small inputs using quicksort is not yet beneficial, and two
+    // small-sorts plus a single merge outperforms it, so use eager mode.
     let eager_sort = len <= T::SMALL_SORT_THRESHOLD * 2;
     drift::sort(v, scratch, eager_sort, is_less);
 }
 
 trait BufGuard<T> {
     fn with_capacity(capacity: usize) -> Self;
-    fn mut_ptr(&mut self) -> *mut T;
-    fn capacity(&self) -> usize;
+    fn as_mut_uninit_slice(&mut self) -> &mut [MaybeUninit<T>];
 }
 
 impl<T> BufGuard<T> for Vec<T> {
     fn with_capacity(capacity: usize) -> Self {
         Vec::with_capacity(capacity)
     }
-    fn mut_ptr(&mut self) -> *mut T {
-        self.as_mut_ptr()
-    }
-    fn capacity(&self) -> usize {
-        self.capacity()
+    fn as_mut_uninit_slice(&mut self) -> &mut [MaybeUninit<T>] {
+        self.spare_capacity_mut()
     }
 }
 
@@ -154,11 +133,11 @@ impl<T, const N: usize> AlignedStorage<T, N> {
         }
     }
 
-    fn as_mut(&mut self) -> &mut [MaybeUninit<T>] {
+    fn as_mut_uninit_slice(&mut self) -> &mut [MaybeUninit<T>] {
         let len = N / mem::size_of::<T>();
 
         // SAFETY: `_align` ensures we are correctly aligned.
-        unsafe { &mut *ptr::slice_from_raw_parts_mut(self.storage.as_mut_ptr().cast(), len) }
+        unsafe { core::slice::from_raw_parts_mut(self.storage.as_mut_ptr().cast(), len) }
     }
 }
 
@@ -166,13 +145,7 @@ impl<T, const N: usize> AlignedStorage<T, N> {
 
 /// # Safety
 ///
-/// This is an internal trait that must match Rust
-/// interior mutability rules.
-///
-/// Can the type have interior mutability, this is checked by testing if T is Freeze. If the type can
-/// have interior mutability it may alter itself during comparison in a way that must be observed
-/// after the sort operation concludes. Otherwise a type like Mutex<Option<Box<str>>> could lead to
-/// double free.
+/// This is an internal trait that must match Rust interior mutability rules.
 ///
 /// Direct copy of stdlib internal implementation of Freeze.
 pub(crate) unsafe auto trait Freeze {}
@@ -198,9 +171,8 @@ impl<T: Freeze> const IsFreeze for T {
 
 #[must_use]
 const fn has_direct_interior_mutability<T>() -> bool {
-    // Can the type have interior mutability, this is checked by testing if T is Freeze. If the type
-    // can have interior mutability it may alter itself during comparison in a way that must be
-    // observed after the sort operation concludes. Otherwise a type like Mutex<Option<Box<str>>>
-    // could lead to double free.
+    // If a type has interior mutability it may alter itself during comparison
+    // in a way that must be preserved after the sort operation concludes.
+    // Otherwise a type like Mutex<Option<Box<str>>> could lead to double free.
     !T::IS_FREEZE
 }
